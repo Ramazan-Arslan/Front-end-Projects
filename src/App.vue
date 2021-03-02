8ed7efd2af3e59d849ef9f34f25436fdc8c9e1b5
Ramazan Arslan <ramo@Ramazan-MacBook-Air.local>
2021-03-02 17:03:00 +0300

    done

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,10 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
 import Todo from './components/Todo.vue';
 
 @Component({
   components: {
-    HelloWorld,
     Todo,
   },
 })
@@ -20,11 +18,7 @@ export default class App extends Vue {}
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 10%;
   margin-left:20%;
   margin-right:20%;
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="container__header"> {{"TO-DO LİST" }}</div>
+    <div class="container__header"> {{"TO-DO LIST" }}</div>
     <input class ="container__input-task" type="text" placeholder="Enter a new task" v-model="newTodo"  @keyup.enter="addTodo">
     <div class ="container__task-container" v-for="(todo,index) in todos" :key="todo.id">
         <input class ="container__checkbox"  type="checkbox" v-model="todo.finished">
@@ -65,108 +65,7 @@ export default class Todo extends Vue {
     }
 
 }
-
-
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.container{
-  background: #FFFFFF;
-  border: 1px solid #E2E1E1;
-  box-shadow: 2px 2px 4px rgba(215, 215, 215, 0.5);
-  border-radius: 8px;
-  font-family: sans-serif;
-  
-
-  &__header{
-    border: 1px solid #E2E1E1;
-    box-shadow: 2px 2px 4px rgba(215, 215, 215, 0.5);
-    font-size: 30px;
-    line-height: 36px;
-    padding: 1.5%;
-    color: #333333;
-
-  }
-  &__input-task{
-    border: 1px solid #E2E1E1;
-    box-shadow: 2px 2px 4px rgba(215, 215, 215, 0.5);
-    border-radius: 8px;
-    width: 68%;
-    font-size: 20px;
-    line-height: 24px;
-    text-align: center;
-    color: #333333;
-    margin-top: 3%;
-    margin-bottom: 3%;
-    padding: 1%;
-  }
-
-  &__task-container{
-    display: grid;
-    grid-template-columns: 15% 45px auto 10px 45px 15%;
-    margin-bottom: 2%;
-   
-  }
-  &__checkbox{
-    grid-column: 2/3;
-    width: 80%;
-    height: 80%;
-    
-   
-
-    
-  }
-  &__task{
-    grid-column: 3/4;
-    background: #F9F9F9;
-    border: 1px solid #E2E1E1;
-    box-shadow: 2px 2px 4px rgba(215, 215, 215, 0.5);
-    border-radius: 8px;
-    font-size: 12px;
-    font-weight: 600;
-    padding: 3%;
-    color: #333333;
-
-  }
-  &__task2{
-    font-style: italic;
-    text-decoration: line-through;
-    color:grey;
-
-  }
-  &__delete-task{
-    grid-column: 5/6;
-    color:#FFFFFF;
-    background: #C41D1D;
-    border: none;
-    border-radius: 16%;
-    cursor: pointer;
-    &:hover{
-      background-color:gray; 
-      width: 103%;
-      height: 103%;
-    }
-    
-  }
-
-  &__editing-task{
-    grid-column: 3/4;
-    background: #F9F9F9;
-    border: 1px solid #E2E1E1;
-    box-shadow: 2px 2px 4px rgba(215, 215, 215, 0.5);
-    border-radius: 8px;
-    font-size: 12px;
-    font-weight: 600;
-    padding: 3%;
-    color: #333333;
-    text-align: center;
-     &:enabled{
-      background-color:white ;
-    }
-
-  
-  }
-}
-
+@import '../style/Todo.scss';
 </style>
